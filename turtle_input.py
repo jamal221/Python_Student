fe1ef8698d561f8758bde19116ed8b3092abf517
jamal azizbeigi <jamal.azizbeigi589@gmail.com>
2024-01-20 23:00:04 +0330

    add turtle fillcolor generate shapes with different color

--- turtle_input.py
+++ turtle_input.py
@@ -1,13 +1,21 @@
 import turtle
+import random
 t1=turtle.Turtle()
 shape=int(input("لطفا تعدا اظلاع را درج نماييد"))
 shape_num=int(input("چه تعداد از اين شکل را مي خواهيد تکرار نماييد."))
 angle1=360/shape
 angle2=360/shape_num
 for i in range(shape_num):
+    r=random.random()
+    g=random.random()
+    b=random.random()
+    t1.pencolor((r,g,b))
+    t1.fillcolor((r,g,b))
+    t1.begin_fill() 
     for j in range(shape):
         t1.forward(50)
         t1.left(angle1)
+    t1.end_fill()  
     t1.left(angle2)
     
     
